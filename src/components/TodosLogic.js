3274faa0ddbf9075dcbd662e94744d50f4d50d0a
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-13 19:35:58 +0100

    Implement Local Storage

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,27 +1,11 @@
 import React from 'react'
-import { useState } from 'react';
 import InputTodo from './InputTodo'
 import TodosList from './TodosList'
 import { v4 as uuidv4 } from "uuid";
+import { useState, useEffect } from 'react';
 
 const TodosLogic = () => {
-    const [todos, setTodos] = useState([
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ]);
+    const [todos, setTodos] = useState(getInitialTodos());
     const handleChange = (id) => {
         setTodos((prevState) =>
           prevState.map((todo) => {
@@ -60,6 +44,17 @@ const TodosLogic = () => {
           })
         );
       };
+      useEffect(() => {
+        // storing todos items
+        const temp = JSON.stringify(todos);
+        localStorage.setItem('todos', temp);
+      }, [todos]);
+      function getInitialTodos() {
+        // getting stored items
+        const temp = localStorage.getItem('todos');
+        const savedTodos = JSON.parse(temp);
+        return savedTodos || [];
+      }
   return (
     <div>
         <InputTodo addTodoItem={addTodoItem} />
